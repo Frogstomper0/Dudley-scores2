fb5a2b549a261ca0db6b1a4f44ccee0a8987d134
Frogstomper0 <nelsonjackanderson@gmail.com>
2025-08-13 01:09:17 +1000

    Update and rename hello.ts to hello.js

---
+++ api/hello.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+  res.setHeader("content-type", "application/json");
+  res.status(200).send(JSON.stringify({ ok: true }));
+}
+
--- api/hello.ts
+++
@@ -1,5 +0,0 @@
-export default function handler(request: Request): Response {
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { "content-type": "application/json" }
-  });
-}
