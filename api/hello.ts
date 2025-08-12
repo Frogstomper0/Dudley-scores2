b9e608560fa07b9fe9c182acadceabae447e7940
Frogstomper0 <nelsonjackanderson@gmail.com>
2025-08-13 01:14:40 +1000

    Update and rename hello.js to hello.ts

---
+++ api/hello.ts
@@ -0,0 +1,7 @@
+export const config = { runtime: "edge" };
+
+export default function handler(request: Request): Response {
+  return new Response(JSON.stringify({ ok: true }), {
+    headers: { "content-type": "application/json" }
+  });
+}
